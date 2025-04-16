c1a383af4111362a096e503f6a7a909610cd4b54
Kayque Goldner <kayquegoldner@gmail.com>
2025-04-16 06:21:23 -0300

    ai task generation

--- src/routes/actions/project-action.ts
+++ src/routes/actions/project-action.ts
@@ -15,6 +15,7 @@ import generateProjectTasks from "@/api/google-ai";
  */
 const APPWRITE_DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const APPWRITE_PROJECTS_ID = import.meta.env.VITE_APPWRITE_PROJECTS_ID;
+const APPWRITE_TASKS_ID = import.meta.env.VITE_APPWRITE_TASKS_ID;
 
 /**
  * types
@@ -22,11 +23,17 @@ const APPWRITE_PROJECTS_ID = import.meta.env.VITE_APPWRITE_PROJECTS_ID;
 import type { ActionFunction } from "react-router";
 import type { ProjectForm } from "@/types";
 import type { Models } from "appwrite";
+type AiGenTask = {
+  content: string;
+  due_date: string;
+};
 
 const createProject = async (data: ProjectForm) => {
   let project: Models.Document | null = null;
   const aiTaskGen = data.ai_task_gen;
   const taskGenPrompt = data.task_gen_prompt;
+
+  let aiGeneratedTasks: AiGenTask[] = [];
 
   try {
     project = await databases.createDocument(
@@ -44,9 +51,41 @@ const createProject = async (data: ProjectForm) => {
     console.log("Error creating project", error);
   }
 
-  const tasks = await generateProjectTasks(taskGenPrompt);
-  console.log(tasks);
+  if (aiTaskGen) {
+    if (!taskGenPrompt) {
+      throw new Error("Task generation prompt is required");
+    }
 
+    try {
+      aiGeneratedTasks = JSON.parse(
+        (await generateProjectTasks(taskGenPrompt)) ||
+          `[{content: 'Tasks generation failed', due_date: ${new Date()}}]`,
+      );
+    } catch (error) {
+      console.log("Error generating tasks", error);
+    }
+  }
+
+  if (aiGeneratedTasks.length > 0) {
+    const promises = aiGeneratedTasks.map((task) =>
+      databases.createDocument(
+        APPWRITE_DATABASE_ID,
+        APPWRITE_TASKS_ID,
+        generateID(),
+        {
+          ...task,
+          project: project?.$id,
+          userId: getUserId(),
+        },
+      ),
+    );
+
+    try {
+      await Promise.all(promises);
+    } catch (error) {
+      console.log("Error creating tasks", error);
+    }
+  }
   return redirect(`/app/projects/${project?.$id}`);
 };
 
