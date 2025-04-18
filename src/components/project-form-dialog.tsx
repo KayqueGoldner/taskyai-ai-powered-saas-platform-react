10598e3d16aa4bdbf81f54c73dd233b4d3b4bb44
Kayque Goldner <kayquegoldner@gmail.com>
2025-04-18 07:02:53 -0300

    toast create and update project

--- src/components/project-form-dialog.tsx
+++ src/components/project-form-dialog.tsx
@@ -5,10 +5,20 @@ import { useState } from "react";
 import { useFetcher } from "react-router";
 
 /**
+ * custom modules
+ */
+import { truncateString } from "@/lib/utils";
+
+/**
  * components
  */
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { ProjectForm } from "@/components/project-form";
+
+/**
+ * hooks
+ */
+import { useToast } from "@/hooks/use-toast";
 
 /**
  * types
@@ -28,6 +38,7 @@ export const ProjectFormDialog: React.FC<ProjectFormDialogProps> = ({
 }) => {
   const [open, setOpen] = useState(false);
   const fetcher = useFetcher();
+  const { toast } = useToast();
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -39,10 +50,28 @@ export const ProjectFormDialog: React.FC<ProjectFormDialogProps> = ({
           onSubmit={async (data) => {
             setOpen(false);
 
+            const { id, update } = toast({
+              title: `Project ${
+                method === "POST" ? "creating" : "updating"
+              } project...`,
+              duration: Infinity,
+            });
+
             await fetcher.submit(JSON.stringify(data), {
               method: method,
               action: "/app/projects",
               encType: "application/json",
+            });
+
+            update({
+              id,
+              title: `Project ${
+                method === "POST" ? "created" : "updated"
+              } successfully`,
+              description: `The project ${truncateString(data.name, 32)} ${data.ai_task_gen ? "and its tasks" : ""} have been successfully ${
+                method === "POST" ? "created" : "updated"
+              }.`,
+              duration: 5000,
             });
           }}
           onCancel={() => setOpen(false)}
