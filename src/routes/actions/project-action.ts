ed90a8d5610a5d08a4bd2cf99c692fd234a9b691
Kayque Goldner <kayquegoldner@gmail.com>
2025-04-20 11:57:20 -0300

    handle project edit

--- src/routes/actions/project-action.ts
+++ src/routes/actions/project-action.ts
@@ -89,6 +89,29 @@ const createProject = async (data: ProjectForm) => {
   return redirect(`/app/projects/${project?.$id}`);
 };
 
+const updateProject = async (data: ProjectForm) => {
+  const documentId = data.id;
+
+  if (!documentId) {
+    throw new Error("Project ID is required");
+  }
+
+  try {
+    return await databases.updateDocument(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_PROJECTS_ID,
+      documentId,
+      {
+        name: data.name,
+        color_name: data.color_name,
+        color_hex: data.color_hex,
+      },
+    );
+  } catch (error) {
+    console.log("Error updating project", error);
+  }
+};
+
 const deleteProject = async (data: Project) => {
   const documentId = data.id;
 
@@ -115,11 +138,15 @@ const projectAction: ActionFunction = async ({ request }) => {
     return await createProject(data);
   }
 
+  if (method === "PUT") {
+    return await updateProject(data);
+  }
+
   if (method === "DELETE") {
     return await deleteProject(data);
   }
 
-  return null;
+  throw new Error("Invalid method");
 };
 
 export default projectAction;
